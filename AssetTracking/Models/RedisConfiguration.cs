653419183226bc183e5598db6ca2d0c158e43908
Sidney Andrews <sidney@seesharprun.net>
2017-03-09 21:56:10 -0500

    Seach Implementation

---
+++ AssetTracking/Models/RedisConfiguration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace AssetTracking.Models
+{
+    public class RedisConfiguration
+    {
+        const string PREFIX = "REDIS";
+
+        public string Host { get; set; }
+
+        public string Key { get; set; }
+
+        public string Index { get; set; }
+
+        public string Collection { get; set; }
+
+        public static SearchConfiguration RetrieveAppSettings()
+        {
+            return new SearchConfiguration
+            {
+                Host = ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Host)}"],
+                Key = ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Key)}"],
+                Index = ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Index)}"],
+                Collection = ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Collection)}"]
+            };
+        }
+
+        public static void SaveAppSettings(SearchConfiguration config)
+        {
+            ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Host)}"] = config.Host;
+            ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Key)}"] = config.Key;
+            ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Index)}"] = config.Index;
+            ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Collection)}"] = config.Collection;
+        }
+
+        public bool Validate()
+        {
+            if (String.IsNullOrWhiteSpace(Host))
+            {
+                return false;
+            }
+            else if (String.IsNullOrWhiteSpace(Key))
+            {
+                return false;
+            }
+            else if (String.IsNullOrWhiteSpace(Database))
+            {
+                return false;
+            }
+            else if (String.IsNullOrWhiteSpace(Collection))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
--- AssetTracking/Models/SearchConfiguration.cs
+++ AssetTracking/Models/SearchConfiguration.cs
@@ -5,50 +5,34 @@ namespace AssetTracking.Models
 {
     public class SearchConfiguration
     {
-        const string PREFIX = "DOCUMENTDB";
+        const string PREFIX = "SEARCH";
 
-        public string Host { get; set; }
+        public string Name { get; set; }
 
         public string Key { get; set; }
-
-        public string Database { get; set; }
-
-        public string Collection { get; set; }
         
         public static SearchConfiguration RetrieveAppSettings()
         {
             return new SearchConfiguration
             {
-                Host = ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Host)}"],
-                Key = ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Key)}"],
-                Database = ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Database)}"],
-                Collection = ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Collection)}"]
+                Name = ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Name)}"],
+                Key = ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Key)}"]
             };
         }
 
         public static void SaveAppSettings(SearchConfiguration config)
         {
-            ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Host)}"] = config.Host;
+            ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Name)}"] = config.Name;
             ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Key)}"] = config.Key;
-            ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Database)}"] = config.Database;
-            ConfigurationManager.AppSettings[$"{PREFIX}_{nameof(Collection)}"] = config.Collection;
         }
 
         public bool Validate()
         {
-            if (String.IsNullOrWhiteSpace(Host))
+            if (String.IsNullOrWhiteSpace(Name))
             {
                 return false;
             }
             else if (String.IsNullOrWhiteSpace(Key))
-            {
-                return false;
-            }
-            else if (String.IsNullOrWhiteSpace(Database))
-            {
-                return false;
-            }
-            else if (String.IsNullOrWhiteSpace(Collection))
             {
                 return false;
             }
